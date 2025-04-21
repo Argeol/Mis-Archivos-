01e166b76ad734b4e3e9744d0ade4b49993e861b
Argeol <elzurdojr.15@gmail.com>
2025-04-21 15:11:10 -0500

    Gerente de argeol

--- Frontend/src/lib/axiosInstance.js
+++ Frontend/src/lib/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
  const axiosInstance = axios.create({
-     baseURL: 'http://192.168.184.38:5000/',
+     baseURL: 'http://localhost:5158/',
      Headers:{
          'acept': '*/*',
          'Content-Type': 'application/json',
