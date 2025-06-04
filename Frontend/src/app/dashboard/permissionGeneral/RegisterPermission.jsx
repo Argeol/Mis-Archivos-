6e0081e6ce9a14521bbcc93059ab68cecdc87ea3
unknown <danielcamilo02003@gmail.com>
2025-06-04 14:30:47 -0500

    Botones y logo

--- Frontend/src/app/dashboard/permissionGeneral/RegisterPermission.jsx
+++ Frontend/src/app/dashboard/permissionGeneral/RegisterPermission.jsx
@@ -327,9 +327,39 @@ export default function RegisterPermission({ onSuccess }) {
 
       {/* Botón de envío */}
       <div className="pt-4">
-        <Button type="submit" className="w-full">
-          Registrar Permiso
-        </Button>
+       <Button
+        type="submit"
+        disabled={mutation.isLoading}
+        className="mt-4 bg-blue-500 text-white hover:bg-blue-600 transition-colors duration-200 rounded-md mx-auto block w-full flex items-center justify-center gap-2"
+      >
+        {mutation.isLoading ? (
+          <>
+            <svg
+              className="animate-spin h-5 w-5 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+              ></path>
+            </svg>
+            Registrando...
+          </>
+        ) : (
+          <>Registrar Permiso</>
+        )}
+      </Button>
       </div>
 
       {/* Nota final */}
